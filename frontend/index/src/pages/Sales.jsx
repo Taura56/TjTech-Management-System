3ee95e27751ed510d399abb6fe439aa10e6badb7
Taura56 <tauraanderson56@gmail.com>
2026-09-22 18:46:01 +0300

    New updates

--- frontend/index/src/pages/Sales.jsx
+++ frontend/index/src/pages/Sales.jsx
@@ -19,12 +19,16 @@ function Sales() {
     const [editingSale, setEditingSale] = useState(null);
 
     // Form data
-    const [newSale, setNewSale] = useState({
-        customer: "",
-        product: "",
-        quantity: 1,
-        price: 0
-    });
+const [newSale, setNewSale] = useState({
+    customer: "",
+    product: "",
+    quantity: 1,
+    price: 0,
+    payment: "",
+    phone: "",
+    transactionCode: "",
+    paymentStatus: "Pending"
+});
 const [products, setProducts] = useState([]);
 const [customers, setCustomers] = useState([]);
 
@@ -52,17 +56,18 @@ useEffect(() => {
 
 
     // Get product price
-    function getProductPrice(product) {
-        if (product === "Laptop") {
-            return 850;
-        }
+   function getProductPrice(productName) {
 
-        if (product === "Mouse") {
-            return 20;
-        }
+    const product = products.find(
+        (item) => item.name === productName
+    );
 
-        return 0;
+    if (product) {
+        return Number(product.sellingPrice);
     }
+
+    return 0;
+}
 
     // Open modal for adding a new sale
     function handleAddSale() {
@@ -72,7 +77,11 @@ useEffect(() => {
             customer: "",
             product: "",
             quantity: 1,
-            price: 0
+            price: 0,
+            payment: "",
+            phone: "",
+            transactionCode: "",
+            paymentStatus: "Pending"
         });
 
         setShowForm(true);
@@ -84,7 +93,11 @@ useEffect(() => {
             customer: sale.customer,
             product: sale.product,
             quantity: sale.quantity,
-            price: sale.price
+            price: sale.price,
+            payment: sale.payment,
+            phone: sale.phone,
+            transactionCode: sale.transactionCode,
+            paymentStatus: sale.paymentStatus
         });
 
         setEditingSale(sale);
@@ -92,37 +105,113 @@ useEffect(() => {
     }
 
     // Save new sale or update existing sale
-    function handleSaveSale() {
+    function handleSaveSale() {  
         if (
             !newSale.customer ||
             !newSale.product ||
+            !newSale.payment ||
             newSale.quantity <= 0
         ) {
             alert("Please fill in all fields.");
             return;
         }
+        //Requre phone number for M-Pesa payment
+        if (
+            newSale.payment === "M-Pesa" &&
+            !newSale.phone
+        ) {
+            alert("Phone number is required for M-Pesa payment.");
+            return;
+        }
+        // Require transaction code for Card payment
+        if (
+            newSale.payment === "Card" &&
+            !newSale.transactionCode
+        ) {
+            alert("Transaction code is required for Card payment.");
+            return;
+        }
 
         const price = getProductPrice(newSale.product);
+            const selectedProduct = products.find(
+                (product) => product.name === newSale.product
+            );
+
+            if (!selectedProduct) {
+                alert("Product not found.");
+                return;
+            }
+
+            if (newSale.quantity > selectedProduct.stock) {
+                alert("Not enough stock available.");
+                return;
+            }
 
         // UPDATE EXISTING SALE
-        if (editingSale) {
-            const updatedSales = sales.map((sale) => {
-                if (sale.id === editingSale.id) {
-                    return {
-                        ...sale,
-                        customer: newSale.customer,
-                        product: newSale.product,
-                        quantity: newSale.quantity,
-                        price: price,
-                        total: price * newSale.quantity
-                    };
-                }
+        // UPDATE EXISTING SALE
+if (editingSale) {
 
-                return sale;
-            });
+    const oldQuantity = editingSale.quantity;
+    const newQuantity = newSale.quantity;
 
-            setSales(updatedSales);
 
+    // Calculate difference
+    const quantityDifference = newQuantity - oldQuantity;
+
+
+    // Update stock
+    const updatedProducts = products.map((product) => {
+
+        if (product.name === newSale.product) {
+
+            return {
+                ...product,
+                stock: product.stock - quantityDifference
+            };
+
+        }
+
+        return product;
+
+    });
+
+
+    setProducts(updatedProducts);
+
+    localStorage.setItem(
+        "products",
+        JSON.stringify(updatedProducts)
+    );
+
+
+                // Update sale record
+                const updatedSales = sales.map((sale) => {
+
+                    if (sale.id === editingSale.id) {
+
+                        return {
+                            ...sale,
+                            customer: newSale.customer,
+                            product: newSale.product,
+                            quantity: newQuantity,
+                            price: price,
+                            total: price * newQuantity,
+                            payment: newSale.payment,
+                            phone: newSale.phone,
+                            transactionCode: newSale.transactionCode,
+                            paymentStatus: newSale.paymentStatus
+                        };
+
+                    }
+
+                    return sale;
+
+                });
+
+
+                setSales(updatedSales);
+
+    
         } else {
             // ADD NEW SALE
 
@@ -138,18 +227,49 @@ useEffect(() => {
                 quantity: newSale.quantity,
                 price: price,
                 total: price * newSale.quantity,
+                payment: newSale.payment,
+                phone: newSale.phone,
+                transactionCode: newSale.transactionCode,
+                paymentStatus: "paid",
                 date: new Date().toLocaleDateString()
             };
 
             setSales([...sales, sale]);
-        }
+                            // Reduce product stock
+                const updatedProducts = products.map((product) => {
+
+                    if (product.name === newSale.product) {
+
+                        return {
+                            ...product,
+                            stock: product.stock - newSale.quantity
+                        };
+
+                    }
+
+                    return product;
+
+                });
+
+                // Save updated products
+                setProducts(updatedProducts);
+
+                localStorage.setItem(
+                    "products",
+                    JSON.stringify(updatedProducts)
+                );
+                        }
 
         // Reset everything
         setNewSale({
             customer: "",
             product: "",
             quantity: 1,
-            price: 0
+            price: 0,
+            payment: "",
+            phone: "",
+            transactionCode: "",
+            paymentStatus: ""
         });
 
         setEditingSale(null);
@@ -157,19 +277,57 @@ useEffect(() => {
     }
 
     // Delete sale
-    function handleDelete(id) {
-        const confirmDelete = window.confirm(
-            "Are you sure you want to delete this sale?"
-        );
+            function handleDelete(id) {
 
-        if (confirmDelete) {
-            const updatedSales = sales.filter(
-                (sale) => sale.id !== id
-            );
+                const confirmDelete = window.confirm(
+                    "Are you sure you want to delete this sale?"
+                );
 
-            setSales(updatedSales);
-        }
-    }
+                if (confirmDelete) {
+
+                    // Find the sale being deleted
+                    const deletedSale = sales.find(
+                        (sale) => sale.id === id
+                    );
+
+
+                    // Return stock back to products
+                    const updatedProducts = products.map((product) => {
+
+                        if (product.name === deletedSale.product) {
+
+                            return {
+                                ...product,
+                                stock: product.stock + deletedSale.quantity
+                            };
+
+                        }
+
+                        return product;
+
+                    });
+
+
+                    // Update products
+                    setProducts(updatedProducts);
+
+                    localStorage.setItem(
+                        "products",
+                        JSON.stringify(updatedProducts)
+                    );
+
+
+                    // Remove sale
+                    const updatedSales = sales.filter(
+                        (sale) => sale.id !== id
+                    );
+
+
+                    setSales(updatedSales);
+
+                }
+            }
+    
 
     // Cancel form
     function handleCancel() {
@@ -177,7 +335,11 @@ useEffect(() => {
             customer: "",
             product: "",
             quantity: 1,
-            price: 0
+            price: 0,
+            payment: "",
+            phone: "",
+            transactionCode: "",
+            paymentStatus: ""
         });
 
         setEditingSale(null);
@@ -261,6 +423,9 @@ useEffect(() => {
                                 <th>Quantity</th>
                                 <th>Price</th>
                                 <th>Total</th>
+                                <th>Payment</th>
+                                <th>Phone/Reference</th>
+                                <th>Payment Status</th>
                                 <th>Date</th>
                                 <th>Actions</th>
                             </tr>
@@ -271,7 +436,7 @@ useEffect(() => {
                             {sales.length === 0 ? (
 
                                 <tr>
-                                    <td colSpan="8">
+                                    <td colSpan="11">
                                         No sales data available.
                                     </td>
                                 </tr>
@@ -304,6 +469,18 @@ useEffect(() => {
                                             KSh {sale.total}
                                         </td>
 
+                                        <td>
+                                            {sale.payment}
+                                        </td>
+
+                                        <td>
+                                            {sale.phone||sale.transactionCode}
+                                        </td>
+
+                                        <td>
+                                            {sale.paymentStatus}
+                                        </td>
+                                        
                                         <td>
                                             {sale.date}
                                         </td>
@@ -376,6 +553,9 @@ useEffect(() => {
                                         })
                                     }
                                 >
+                                    <option value="">
+                                        Select Customer
+                                    </option>
                                     {customers.map((customer) => (
                                         <option
                                             key={customer.id}
@@ -406,6 +586,9 @@ useEffect(() => {
                                         })
                                     }
                                 >
+                                    <option value="">
+                                        Select Product
+                                    </option>
 
                                     {products.map((product) => (
                                         <option
@@ -441,10 +624,82 @@ useEffect(() => {
                                         })
                                     }
                                 />
-
                             </div>
+                                {/* PAYMENT METHOD */}
 
+                                        <div className="form-group">
 
+                                        <label>
+                                        Payment Method
+                                        </label>
+
+                                        <select
+                                        value={newSale.payment}
+                                        onChange={(e)=>
+                                        setNewSale({
+                                            ...newSale,
+                                            payment:e.target.value
+                                        })
+                                        }
+                                        >
+
+                                        <option value="">
+                                        Select Payment
+                                        </option>
+
+                                        <option value="Cash">
+                                        Cash
+                                        </option>
+
+                                        <option value="M-Pesa">
+                                        M-Pesa
+                                        </option>
+
+                                        <option value="Card">
+                                        Card
+                                        </option>
+
+                                        </select>
+
+                                        </div>
+
+                            {/* PAYMENT FIELD for MPESA */}
+                                    {newSale.payment === "M-Pesa" && (
+                                        <div className="form-group">
+                                            <label>Phone Number</label>
+
+                                            <input
+                                                type="text"
+                                                placeholder="254712345678"
+                                                value={newSale.phone}
+                                                onChange={(e) =>
+                                                    setNewSale({
+                                                        ...newSale,
+                                                        phone: e.target.value
+                                                    })
+                                                }
+                                            />
+                                        </div>
+                                    )}
+                                    
+                                         {/*PAYMENT FIELD for CARD */}  
+                                          {newSale.payment === "Card" && (
+                                                <div className="form-group">
+                                                    <label>Card Reference</label>
+
+                                                    <input
+                                                        type="text"
+                                                        placeholder="Authorization Number"
+                                                        value={newSale.transactionCode}
+                                                        onChange={(e) =>
+                                                            setNewSale({
+                                                                ...newSale,
+                                                                transactionCode: e.target.value
+                                                            })
+                                                        }
+                                                    />
+                                                </div>
+                                            )}
                             {/* FORM BUTTONS */}
                             <div className="form-buttons">
 
